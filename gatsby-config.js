fa52ee929d0b82f323d2c78376d590a20b6c02a6
Nutod <alukodotun@gmail.com>
2019-02-09 13:43:22 +0100

    Added personal information and site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Oladotun Aluko`,
+    description: `Oladotun Aluko's personal website and blog`,
+    author: `Oladotun Aluko`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
