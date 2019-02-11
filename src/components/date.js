7af2dd2b000459753458fef011a1c1f33c6a0e82
Nutod <alukodotun@gmail.com>
2019-02-11 09:55:06 +0100

    Added months array to Date Component

---
+++ src/components/date.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+export default function Date() {
+	return <div />;
+}
