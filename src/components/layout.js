654c71f641e3badef4d581064a0ce0a4e9ba32c2
Nutod <alukodotun@gmail.com>
2019-02-12 14:24:41 +0100

    Removed Date Component

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,9 +32,7 @@ const Layout = ({ children }) => (
 					>
 						<main>{children}</main>
 						<footer>
-							© {new Date().getFullYear()}, Built with
-							{` `}
-							<a href="https://www.gatsbyjs.org">Gatsby</a>
+							<p>Oladotun Aluko</p>
 						</footer>
 					</div>
 				</>
