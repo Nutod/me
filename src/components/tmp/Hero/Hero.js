00d73abc4bbd378e58c5aab1f38e89516b38fc80
dotunalukosprinble <dotuna@sprinble.co>
2019-03-04 22:30:34 +0100

    Remove unused file

--- src/components/tmp/Hero/Hero.js
+++ src/components/tmp/Hero/Hero.js
@@ -6,18 +6,18 @@ import "./Hero.css";
 import Resume from "../../assets/Résume.pdf";
 
 export default function Hero() {
-	return (
-		<HeroWrapper>
-			<div style={{ width: "80vw" }}>
-				<HeadingThree>Hi! I'm</HeadingThree>
-				<HeroHeader>Oladotun Aluko</HeroHeader>
-				<p style={{ marginTop: "2.7rem" }}>
-					<Button href={Resume} download="Oladotun Aluko Résume">
-						Download Resume
-					</Button>
-				</p>
-			</div>
-			<HeroBackground />
-		</HeroWrapper>
-	);
+  return (
+    <HeroWrapper>
+      <div style={{ width: "80vw" }}>
+        <HeadingThree>Hi! I'm</HeadingThree>
+        <HeroHeader>Oladotun Aluko</HeroHeader>
+        <p style={{ marginTop: "2.7rem" }}>
+          <Button href={Resume} download="Oladotun Aluko Résume">
+            Download Resume
+          </Button>
+        </p>
+      </div>
+      <HeroBackground />
+    </HeroWrapper>
+  );
 }
