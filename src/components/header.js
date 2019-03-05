9c66ec68490d86ea9fa7cf64be991a8fbbfcab3e
dotunalukosprinble <dotuna@sprinble.co>
2019-03-05 22:48:51 +0100

    Removed unused import

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,12 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
-import { HeaderWrapper, LogoWrapper } from "./styles/Header";
-import Logo from "./logo";
+// import { HeaderWrapper, LogoWrapper } from "./styles/Header";
+// import Logo from "./logo";
 
 const Header = ({ siteTitle }) => (
 	<HeaderWrapper>
-		{/* <div />
-		<LogoWrapper>
-			<Link to="/">
-				<div style={{ width: "3.5rem" }}>
-					<Logo />
-				</div>
-			</Link>
-		</LogoWrapper>
-		<div /> */}
+
 	</HeaderWrapper>
 );
 
