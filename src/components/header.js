0ec10ff437016010a04465268e47296a1c61710e
Nutod <alukodotun@gmail.com>
2019-08-09 23:39:46 +0100

    Removed dead code from Layout component

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,14 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
-const Header = ({ siteTitle }) => (
-	<HeaderWrapper>
-
-	</HeaderWrapper>
-);
+const Header = ({ siteTitle }) => <HeaderWrapper />;
 
 Header.propTypes = {
-	siteTitle: PropTypes.string
+  siteTitle: PropTypes.string
 };
 
 Header.defaultProps = {
-	siteTitle: ``
+  siteTitle: ``
 };
 
 export default Header;
