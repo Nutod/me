da254fd6326ed4c9ea66209cbb7b09afd6045c32
Nutod <alukodotun@gmail.com>
2019-08-09 23:34:44 +0100

    Removed unused code logic from Header component

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,6 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
-// import { HeaderWrapper, LogoWrapper } from "./styles/Header";
-// import Logo from "./logo";
 
 const Header = ({ siteTitle }) => (
 	<HeaderWrapper>
